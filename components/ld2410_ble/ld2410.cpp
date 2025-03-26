66eb1d9f5c2f75404273e3486da1cc6ba422fe81
shammysha <65885873+shammysha@users.noreply.github.com>
2025-03-26 11:05:42 +0300

    Next Try

--- components/ld2410_ble/ld2410.cpp
+++ components/ld2410_ble/ld2410.cpp
@@ -16,8 +16,6 @@ namespace ld2410_ble{
 
 static const char *const TAG = "ld2410";
 
-LD2410BLEComponent::LD2410BLEComponent() {}
-
 void LD2410BLEComponent::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
   switch (event) {
     case ESP_GATTC_OPEN_EVT: {

5834e58c0d134866aa5a23e70a5d8a82a143ad78
shammysha <65885873+shammysha@users.noreply.github.com>
2025-04-03 08:38:02 +0300

    Next Try

--- components/ld2410_ble/ld2410.cpp
+++ components/ld2410_ble/ld2410.cpp
@@ -18,8 +18,13 @@ namespace ld2410_ble{
 
 static const char *const TAG = "ld2410";
 
+void LD2410BLEComponent::void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
+  ESP_LOGI(TAG, "GAP Event received: %d", event);
+
+}
+
 void LD2410BLEComponent::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
-  ESP_LOGI(TAG, "Event received: %d", event);
+  ESP_LOGI(TAG, "GATTS Event received: %d", event);
 
   switch (event) {
 

4d3cc686b957284cbe235eb4ab3a09f8c23e89e1
shammysha <65885873+shammysha@users.noreply.github.com>
2025-04-03 08:47:09 +0300

    Next Try

--- components/ld2410_ble/ld2410.cpp
+++ components/ld2410_ble/ld2410.cpp
@@ -31,6 +31,7 @@ void LD2410BLEComponent::gattc_event_handler(esp_gattc_cb_event_t event, esp_gat
     case ESP_GATTC_OPEN_EVT: {
       if (param->open.status == ESP_GATT_OK) {
         ESP_LOGI(TAG, "Connected successfully!");
+        this->node_state = espbt::ClientState::ESTABLISHED;
         this->set_permissions();
       }
       break;
@@ -92,7 +93,7 @@ void LD2410BLEComponent::gattc_event_handler(esp_gattc_cb_event_t event, esp_gat
           ESP_LOGW(TAG, "Error registering for notifications at handle %d, status=%d", param->reg_for_notify.handle, param->reg_for_notify.status);
           break;
         }
-//        this->node_state = espbt::ClientState::ESTABLISHED;
+        this->node_state = espbt::ClientState::ESTABLISHED;
         ESP_LOGD(TAG, "Register for notify on %s complete", this->char_notify_uuid_.to_string().c_str());
       }
       break;

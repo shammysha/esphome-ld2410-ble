9a7f2a87214d72b62bed31bda05ed198ac82cbe4
shammysha <65885873+shammysha@users.noreply.github.com>
2025-07-23 06:49:10 +0300

    characterisics uuid corrected

--- components/ld2410_ble/ld2410.h
+++ components/ld2410_ble/ld2410.h
@@ -210,8 +210,8 @@ class LD2410BLEComponent : public PollingComponent, public ble_client::BLEClient
 
 
   esp32_ble_tracker::ESPBTUUID service_uuid_ = esp32_ble_tracker::ESPBTUUID::from_raw("0000fff0-0000-1000-8000-00805f9b34fb");
-  esp32_ble_tracker::ESPBTUUID char_notify_uuid_ = esp32_ble_tracker::ESPBTUUID::from_raw("0000fff0-0000-1000-8000-00805f9b34fb");
-  esp32_ble_tracker::ESPBTUUID char_command_uuid_ = esp32_ble_tracker::ESPBTUUID::from_raw("0000fff0-0000-1000-8000-00805f9b34fb");
+  esp32_ble_tracker::ESPBTUUID char_notify_uuid_ = esp32_ble_tracker::ESPBTUUID::from_raw("0000fff1-0000-1000-8000-00805f9b34fb");
+  esp32_ble_tracker::ESPBTUUID char_command_uuid_ = esp32_ble_tracker::ESPBTUUID::from_raw("0000fff2-0000-1000-8000-00805f9b34fb");
 
   esp_gatt_write_type_t write_type_{ESP_GATT_WRITE_TYPE_NO_RSP};
 

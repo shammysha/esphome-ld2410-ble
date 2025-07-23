ca8af74657fa1bc8591f2095dcbc5d40b1a5ef6d
shammysha <65885873+shammysha@users.noreply.github.com>
2025-07-23 20:43:39 +0300

    debugging

--- components/ld2410_ble/__init__.py
+++ components/ld2410_ble/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import ble_client
+from esphome.components import ble_client, ble_sensor
 from esphome.const import CONF_ID, CONF_THROTTLE, CONF_TIMEOUT, CONF_PASSWORD
 from esphome import automation
 from esphome.automation import maybe_simple_id

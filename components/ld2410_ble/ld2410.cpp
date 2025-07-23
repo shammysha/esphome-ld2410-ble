e29e4efe1848c117e28de94be4e56ef2d0a5c138
shammysha <65885873+shammysha@users.noreply.github.com>
2025-07-23 09:05:11 +0300

    debugging

--- components/ld2410_ble/ld2410.cpp
+++ components/ld2410_ble/ld2410.cpp
@@ -75,32 +75,6 @@ void LD2410BLEComponent::gattc_event_handler(esp_gattc_cb_event_t event, esp_gat
       break;
     }
 
-    case ESP_GATTC_SEARCH_CMPL_EVT: {
-      esphome::ble_client::BLECharacteristic *chr;
-
-      chr = this->parent()->get_characteristic(this->service_uuid_, this->char_command_uuid_);
-      if (chr == nullptr) {
-        ESP_LOGE(TAG, "[%s] No command service found at device. Does it really LD2410?", this->parent()->address_str().c_str());
-        break;
-      }
-      this->char_command_handle_ = chr->handle;
-
-      chr = this->parent()->get_characteristic(this->service_uuid_, this->char_notify_uuid_);
-      if (chr == nullptr) {
-        ESP_LOGE(TAG, "[%s] No notify service found at device. Does it really LD2410?", this->parent()->address_str().c_str());
-        break;
-      }
-      this->char_notify_handle_ = chr->handle;
-
-      auto status = esp_ble_gattc_register_for_notify(this->parent()->get_gattc_if(), this->parent()->get_remote_bda(), this->char_notify_handle_);
-
-      if (status) {
-        ESP_LOGE(TAG, "%s esp_ble_gattc_register_for_notify failed, status=%d", this->parent()->address_str().c_str(), status);
-      }
-
-      break;
-    }
-
     case ESP_GATTC_READ_CHAR_EVT: {
       if (param->read.status != ESP_GATT_OK) {
         ESP_LOGE(TAG, "Error reading char at handle %d, status=%d", param->read.handle, param->read.status);
